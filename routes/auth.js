864472554781bec743085f10c51c287d5a6b53d3
goldencat-tom <bluebird6922@gmail.com>
2021-11-12 04:00:38 +0300

    first project

---
+++ routes/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('express-jwt');
+const secret = require('../config').secret;
+
+function getTokenFromHeader(req){
+  if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token' ||
+      req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+    return req.headers.authorization.split(' ')[1];
+  }
+
+  return null;
+}
+
+const auth = {
+  required: jwt({
+    secret: secret,
+    userProperty: 'payload',
+    algorithms: ['RS256'],
+    getToken: getTokenFromHeader
+  }),
+  optional: jwt({
+    secret: secret,
+    userProperty: 'payload',
+    algorithms: ['RS256'],
+    credentialsRequired: false,
+    getToken: getTokenFromHeader
+  })
+};
+
+module.exports = auth;
